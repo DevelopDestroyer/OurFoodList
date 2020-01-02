56e4232bb14a3d57575d28c8afda4ccbb8aa983b
DevelopDestroyer <developdestroyer@gmail.com>
2020-01-02 23:26:03 +0900

    API 통신 기능 구현
    맛집 목록 불러오기 기능 구현

--- publishing/src/pages/registRestaurant.vue
+++ publishing/src/pages/registRestaurant.vue
@@ -15,9 +15,8 @@
  	        	v-model="inputKeyword"
                 placeholder="맛집을 검색해주세요">
           </fg-input>
-
-            </fg-input>					  
-		  <n-button v-on:click="getData" style="margin-top:1px;" type="primary" icon round>
+				  
+		  <n-button v-on:click="searchReq()" style="margin-top:1px;" type="primary" icon round>
 			<i class="fa fa-heart"></i>
 		  </n-button>			  
 		  
@@ -32,48 +31,21 @@
 
 
  <card style="width: 100%; color:black; text-align:left">
-   <ul slot="raw-content" class="list-group list-group-flush">
+
+    <ul v-for="item in list" v-bind:key="item.id"
+	  slot="raw-content" class="list-group list-group-flush">
      <li class="list-group-item">
-	   <h5>이태리 부대찌개</h5>
-	   성남시 분당구 판교로 <br/>
+	   <h5>{{item.title}}</h5>
+	   {{item.address}} <br/>
+	   {{item.roadAddress}} <br/>
+	   {{item.category}}<br/>
+	   {{item.mapx}}, {{item.mapx}}<br/>
        <n-button type="primary" size="sm">등록하기</n-button>
      </li> 
    </ul>
-    <ul slot="raw-content" class="list-group list-group-flush">
-     <li class="list-group-item">
-	   <h5>이태리 부대찌개</h5>
-	   성남시 분당구 판교로 <br/>
-       <n-button type="primary" size="sm">등록하기</n-button>
-     </li> 
-   </ul>
-   <ul slot="raw-content" class="list-group list-group-flush">
-     <li class="list-group-item">
-	   <h5>이태리 부대찌개</h5>
-	   성남시 분당구 판교로 <br/>
-       <n-button type="primary" size="sm">등록하기</n-button>
-     </li> 
-   </ul>
-   <ul slot="raw-content" class="list-group list-group-flush">
-     <li class="list-group-item">
-	   <h5>이태리 부대찌개</h5>
-	   성남시 분당구 판교로 <br/>
-       <n-button type="primary" size="sm">등록하기</n-button>
-     </li> 
-   </ul>
-   <ul slot="raw-content" class="list-group list-group-flush">
-     <li class="list-group-item">
-	   <h5>이태리 부대찌개</h5>
-	   성남시 분당구 판교로 <br/>
-       <n-button type="primary" size="sm">등록하기</n-button>
-     </li> 
-   </ul>
-   <ul slot="raw-content" class="list-group list-group-flush">
-     <li class="list-group-item">
-	   <h5>이태리 부대찌개</h5>
-	   성남시 분당구 판교로 <br/>
-       <n-button type="primary" size="sm">등록하기</n-button>
-     </li> 
-   </ul>
+
+
+
    <ul slot="raw-content" class="list-group list-group-flush">
      <li class="list-group-item">
 	   <h5>이태리 부대찌개</h5>
@@ -113,16 +85,85 @@ export default {
   data() {
     return {
 	  inputKeyword : '',
+	  list: [
+		{
+			title: "<b>수진역</b> 8호선",
+			link: "",
+			category: "교통,운수>지하철,전철",
+			description: "",
+			telephone: "02-6311-8251",
+			address: "경기도 성남시 수정구 수진동 2205-1",
+			roadAddress: "경기도 성남시 수정구 산성대로 200",
+			mapx: "324127",
+			mapy: "537592"
+		},
+		{
+			title: "Cafe Kudeta",
+			link: "",
+			category: "음식점>카페,디저트",
+			description: "",
+			telephone: "031-757-6364",
+			address: "경기도 성남시 수정구 수진동 2207",
+			roadAddress: "경기도 성남시 수정구 산성대로 199-1",
+			mapx: "324117",
+			mapy: "537639"
+
+		}
+	  ],
 	  getData: function(){
-	  
+		//alert('보이루');
+	  	
+		/*
         axios.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
+//        axios.get('/api/searchRestaurant.php?keyword=')
           .then(function(response){
             alert("받아온 값 : " + response.data.fe3);
             console.log(response); // 객체 형태로 반환. 파싱작업 불필요
            });
-		   
+		  */ 
       }  
     }
+  },
+  methods: {
+  
+	emptyList(){
+     	this.list = [];
+	},
+    searchReq(){
+	    var vm = this;
+		/*
+        axios.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
+//        axios.get('/api/searchRestaurant.php?keyword=')
+          .then(function(response){
+            alert("받아온 값 : " + response.data.fe3);
+			vm.list = [];
+            console.log(response); // 객체 형태로 반환. 파싱작업 불필요
+           });		
+		*/
+        axios.get('/api/searchRestaurant.php?keyword=' + this.inputKeyword)
+          .then(function(response){
+            //alert("다 받아왓당" + response.items.length);
+			console.log("다 받아왓당" + response.data.items.length);
+            console.log(response);
+			vm.list = [];
+			for(var i = 0; i < response.data.items.length; i++){
+				vm.list.push({
+					title: response.data.items[i].title,
+					link: response.data.items[i].link,
+					category: response.data.items[i].category,
+					description: response.data.items[i].description,
+					telephone: response.data.items[i].telephone,
+					address: response.data.items[i].address,
+					roadAddress: response.data.items[i].roadAddress,
+					mapx: response.data.items[i].mapx,
+					mapy: response.data.items[i].mapy
+				})
+			}
+        });	
+		
+		
+
+	}
   }
 };
 </script>
