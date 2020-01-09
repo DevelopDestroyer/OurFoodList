53f0f0672ee7cea694f1e1768fdb7eb249987505
DevelopDestroyer <developdestroyer@gmail.com>
2020-01-10 01:19:11 +0900

    맛집등록 UI개선, 모달추가

--- publishing/src/pages/registRestaurant.vue
+++ publishing/src/pages/registRestaurant.vue
@@ -1,3 +1,4 @@
+  
 <template>
     <div class="page-header clear-filter" filter-color="orange">
       <parallax
@@ -39,41 +40,99 @@
    	   <i class="now-ui-icons location_bookmark"></i> {{item.category}}<br/><br/>
 	   <badge type="primary">지번</badge> {{item.address}} <br/>
 	   <badge type="primary">도로명</badge> {{item.roadAddress}} <br/>
-	   <input type="hidden" :value=item.mapx></input>
-	   <input type="hidden" :value=item.mapy></input>
+	   <!--input type="hidden" :value=item.mapx></input>
+	   <input type="hidden" :value=item.mapy></input-->
 	   
 	   
-       <n-button type="primary" size="sm">등록하기</n-button>
+       <n-button type="primary" size="sm" @click.native="modals.notice=true">등록하기</n-button>
      </li> 
    </ul>
 
 
-
-   <ul slot="raw-content" class="list-group list-group-flush">
+	<!--샘플카드-->
+   <!--ul slot="raw-content" class="list-group list-group-flush">
      <li class="list-group-item">
 	   <h5>이태리 부대찌개2</h5>
 	   <badge type="primary">도로명</badge> 성남시 분당구 판교로 <br/>
        <n-button type="primary" size="sm">등록하기</n-button>
      </li> 
-   </ul>
+   </ul-->
   
   </card>
 
 		  
 		  </div>
 		  <div class="col-md-1 col-lg-1"></div>
-        </div>		
+        </div>	
+	   
+<template>
+<modal :show.sync="modals.notice"
+             footer-classes="justify-content-center"
+             type="notice" style="color:gray;margin-top:80px;">
+			 <br><br>
+		 
+        <h5 slot="header" class="modal-title">How Do You Become an Affiliate?</h5>
+        <template>
+<div class="col-md-12 col-lg-12" style="border:0px solid white;width:100%;height:400px; overflow-y:scroll;overflow-x:hidden">			
+          <div class="instruction"  style="color:gray">
+            <div class="row">
+              <div class="col-md-8">
+                <strong>1. Register</strong>
+                <p class="description"  style="color:gray">The first step is to create an account at <a
+                  href="http://www.creative-tim.com/">Creative Tim</a>. You can choose a social network or
+                  go for the classic version, whatever works best for you.</p>
+              </div>
+              <div class="col-md-4">
+                <div class="picture">
+                  <img src="/img/bg1.jpg" alt="Thumbnail Image" class="rounded img-raised">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="instruction">
+            <div class="row">
+              <div class="col-md-8">
+                <strong>2. Apply</strong>
+                <p class="description"  style="color:gray">The first step is to create an account at <a
+                  href="http://www.creative-tim.com/">Creative Tim</a>. You can choose a social network or
+                  go for the classic version, whatever works best for you.</p>
+              </div>
+              <div class="col-md-4">
+                <div class="picture">
+                  <img src="/img/bg3.jpg" alt="Thumbnail Image" class="rounded img-raised">
+                </div>
+              </div>
+            </div>
+          </div>
+          <p>If you have more questions, don't hesitate to contact us or send us a tweet @creativetim. We're
+            here to help!</p>
+			</div><!--스크롤 영역 끝-->
+        </template>
+        <div slot="footer" class="justify-content-center">
+          <n-button type="info" round @click.native="modals.notice = false">Sounds good!</n-button>
+        </div>
+
+</modal>
+</template>
+
+		
        </template>
 
+	   
 
+	  
 
       </div>
     </div>
   </div>
+  
+
+  
+  
 </template>
-<!--script src="/js/axios.js"></script-->
+<script src="/js/GeoTrans.js"></script>
 <script>
-import { Card, Button, FormGroupInput, Badge } from '@/components';
+import { Card, Button, FormGroupInput, Badge, Modal } from '@/components';
 import MainFooter from '@/layout/MainFooter';
 const axios = require('axios');
 export default {
@@ -82,6 +141,7 @@ export default {
   components: {
     Card,
     MainFooter,
+	Modal,
     [Button.name]: Button,
     [FormGroupInput.name]: FormGroupInput,
 	[Badge.name]: Badge
@@ -89,17 +149,20 @@ export default {
   data() {
     return {
 	  inputKeyword : '',
+      modals: {
+          notice: true
+      },	  
 	  list: [
 		{
-			title: "수진역 8호선",
+			title: "검색해주세요!",
 			link: "",
-			category: "교통,운수>지하철,전철",
+			category: "",
 			description: "",
-			telephone: "02-6311-8251",
-			address: "경기도 성남시 수정구 수진동 2205-1",
-			roadAddress: "경기도 성남시 수정구 산성대로 200",
-			mapx: "324127",
-			mapy: "537592"
+			telephone: "",
+			address: "",
+			roadAddress: "",
+			mapx: "",
+			mapy: ""
 		},
 		{
 			title: "Cafe Kudeta",
@@ -149,7 +212,14 @@ export default {
             console.log(response);
 			vm.list = [];
 			for(var i = 0; i < response.data.items.length; i++){
-			    var remove_tag_title = response.data.items[i].title;
+			    //카텍좌표계 위경도로 변환
+			    var geo = new GeoTrans();
+				geo.init("katec", "geo");
+				var pt = new Point(response.data.items[i].mapx, response.data.items[i].mapy);
+				var out_pt = geo.conv(pt);
+				
+				//타이틀에 있는 태그 제거
+				var remove_tag_title = response.data.items[i].title;
 				remove_tag_title = remove_tag_title.replace('<b>', '');
 				remove_tag_title = remove_tag_title.replace('</b>', '');
 
@@ -161,8 +231,8 @@ export default {
 					telephone: response.data.items[i].telephone,
 					address: response.data.items[i].address,
 					roadAddress: response.data.items[i].roadAddress,
-					mapx: response.data.items[i].mapx,
-					mapy: response.data.items[i].mapy
+					mapx: out_pt.x,
+					mapy: out_pt.y
 				})
 			}
         });	
