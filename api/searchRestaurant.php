7264dea52f6ce5ab3374da8bd211b573e402f4b2
developdestroyer <developdestroyer@gmail.com>
2020-03-10 02:51:05 +0900

    카텍좌표계 위경도 관련 데이터 정합성 수정, 맵 마커 기능 수정..!!!!!

--- api/searchRestaurant.php
+++ api/searchRestaurant.php
@@ -7,6 +7,7 @@
   $db_user_name = $config['dbusername'];
   $db_name = $config['dbname'];
   $db_pw = $config['dbpw'];
+  $kakaoRestAPIKey = $config['kakaoRestAPIKey'];
   //$response = array();
 
   $encText = urlencode($_GET['keyword']); //검색할 Text를 url인코딩
@@ -16,8 +17,7 @@
     xml ver : https://openapi,naver.com/v1/search/book.xml?...
   */
   $url = "https://openapi.naver.com/v1/search/local.json?query=".$encText;
- 
-//  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // xml 결과
+
   $is_post = false;
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
@@ -54,18 +54,54 @@
 
     //<b> 제거
     for($i=0; $i < count($response['items']); $i++){
+
+        $url2 = "https://dapi.kakao.com/v2/local/geo/transcoord.json?x=".$response['items'][$i]['mapx']."&y=".$response['items'][$i]['mapy']."&input_coord=KTM&output_coord=WGS84";
+
+        $ch2 = curl_init();
+        curl_setopt($ch2, CURLOPT_URL, $url2);
+        curl_setopt($ch2, CURLOPT_POST, false);
+        curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+        $headers2 = array();
+        $headers2[] = "Authorization: KakaoAK ".$kakaoRestAPIKey;
+        curl_setopt($ch2, CURLOPT_HTTPHEADER, $headers2);
+        curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, 0);
+        $response_str2 = curl_exec ($ch2);
+        //echo $response_str."what?";
+        $response2 = json_decode($response_str2,true);
+        $status_code2 = curl_getinfo($ch2, CURLINFO_HTTP_CODE);
+
+        curl_close ($ch2);
+
       $response['items'][$i]['title'] = str_replace("<b>", "", $response['items'][$i]['title']);
       $response['items'][$i]['title'] = str_replace("</b>", "", $response['items'][$i]['title']);
 
       $sql = $sql."('".$response['items'][$i]['mapx']."-".$response['items'][$i]['mapy']."-".$response['items'][$i]['title']."', "; //store_id
+
+
+
+
       $sql = $sql."'".$response['items'][$i]['title']."', "; //store_name
       $sql = $sql."'".$response['items'][$i]['category']."', ";
       $sql = $sql."'".$response['items'][$i]['description']."', ";
       $sql = $sql."'".$response['items'][$i]['telephone']."', ";
       $sql = $sql."'".$response['items'][$i]['address']."', ";
       $sql = $sql."'".$response['items'][$i]['roadAddress']."', ";
-      $sql = $sql."'".$response['items'][$i]['mapx']."', ";
-      $sql = $sql."'".$response['items'][$i]['mapy']."', ";
+      /*위에 카텍좌표계로 store_id를 만들어주고, 그 다음 위경도로 변환된 값들을 db에 저장한다..
+      클라이언트는 카텍좌표계를 json 그대로 이어받아 id를 만들어 리뷰생성을 요청 할 수 있다
+      정리 : store_id에는 카텍좌표계 조합의 id가 생성돼며, lat, lon에는 위경도로 변환된 값이 이용된다...
+      이 lat, lon은 지도화면에서 json으로 조합되어 그대로 내려가며 클라이언트는 이것을 별도 연산없이 그대로 쓰면 된다!
+      */
+      if($status_code2 == 200) {
+        //$response['items'][$i]['mapx'] = $response2['documents'][0]['x'];
+        //$response['items'][$i]['mapy'] = $response2['documents'][0]['y'];
+        $sql = $sql."'".(string)$response2['documents'][0]['x']."', ";
+        $sql = $sql."'".(string)$response2['documents'][0]['y']."', ";
+      }
+      else{
+        $sql = $sql."'".$response['items'][$i]['mapx']."', ";
+        $sql = $sql."'".$response['items'][$i]['mapy']."', ";
+      }
+
       $sql = $sql."'".date("Y-m-d H:i:s")."'), ";
     }
     $sql = substr($sql , 0, -2);
--- publishing/src/pages/Index.vue
+++ publishing/src/pages/Index.vue
@@ -81,6 +81,19 @@
 			  
                 </ul>
              </card>
+			  <input id="closeOverlay" type="button" value="0" v-on:click="closeOverlay()" />
+			  <script>
+				  let overlayCnt = 1000;
+
+				  // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
+				  function closeOverlayClassicJavas() {
+				  	alert("나 여깄어!");document.getElementById('closeOverlay').dispatchEvent(document.createEvent("HTMLEvents").initEvent("click", true, false));
+
+//				  document.getElementById('closeOverlay').value = overlayCnt;
+				  overlayCnt++;
+				  }
+
+			  </script>
            </div>
           </div>		   
 			 
@@ -106,6 +119,7 @@ import DownloadSection from './components/DownloadSection';
 import { Card } from '@/components';
 import { BUS } from './EventBus';
 const axios = require('axios');
+
 
 export default {
   name: 'index',
@@ -136,6 +150,7 @@ export default {
       show: true,
 		mapData: null,
 		mapMarker: null,
+		mapOverlay: null,
 
 		myReviewData: [{
 			store_id: '',
@@ -193,7 +208,8 @@ export default {
 		  // 마커를 생성합니다
 		  this.mapMarker = new kakao.maps.Marker({
 			  map: this.mapData, // 마커를 표시할 지도
-			  position: positions[i].latlng // 마커의 위치
+			  position: positions[i].latlng, // 마커의 위치
+			  isOn: false
 		  });
 
 		  // 마커에 표시할 인포윈도우를 생성합니다
@@ -205,7 +221,7 @@ export default {
 		  // 이벤트 리스너로는 클로저를 만들어 등록합니다
 		  // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
 		  kakao.maps.event.addListener(this.mapMarker, 'click', makeOverListener(this.mapData, this.mapMarker, infowindow));
-		  kakao.maps.event.addListener(this.mapMarker, 'mouseout', makeOutListener(infowindow));
+		  //kakao.maps.event.addListener(this.mapMarker, 'mouseout', makeOutListener(infowindow));
 		  //this.mapData = map;
 	  }
   },
@@ -218,41 +234,139 @@ export default {
 		this.show = true;
 	  }
 	},
+	  closeOverlay() {
+    	  alert("으아아아");
+    	  console.log("으아아아ㅏ아아ㅏㅏ");
+	      this.mapOverlay.setMap(null);
+      },
 	  maptest() {
 
 // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다
-
+		  /*
 		  var positions = [];
-
-		  alert("뷰 리스트 길이 : " + this.myReviewData.length);
 		  for(let i = 0; i < this.myReviewData.length; i++){
 
 			  positions.push({
-				  content: '<div style="color:black;">'+this.myReviewData[i].store_name+'</div>',
+				  content:  '<div style="z-index:10000; width:200px; height:120px; color:black; box-shadow: 5px 5px 5px #BDBDBD; border:0px solid white;">'
+						      + "<p style='font-size:10px;'><b>" + this.myReviewData[i].store_name + "</b></p>"
+							  + "<p style='font-size:8px; color:gray;'>" + this.myReviewData[i].address + "</p>"
+						  + '</div>',
 				  latlng: new kakao.maps.LatLng(this.myReviewData[i].lat, this.myReviewData[i].lon)
 			  })
 		  }
 
-		  alert("포지션즈 리스트 길이 : " + this.myReviewData.length);
 		  for (var i = 0; i < positions.length; i ++) {
 			  // 마커를 생성합니다
 			  this.mapMarker = new kakao.maps.Marker({
 				  map: this.mapData, // 마커를 표시할 지도
-				  position: positions[i].latlng // 마커의 위치
+				  position: positions[i].latlng, // 마커의 위치
+				  isOn: false
 			  });
 
 			  // 마커에 표시할 인포윈도우를 생성합니다
 			  var infowindow = new kakao.maps.InfoWindow({
-				  content: positions[i].content // 인포윈도우에 표시할 내용
+				  content: positions[i].content, // 인포윈도우에 표시할 내용
+				  removable : true
 			  });
+
+
 
 			  // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
 			  // 이벤트 리스너로는 클로저를 만들어 등록합니다
 			  // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
 			  kakao.maps.event.addListener(this.mapMarker, 'click', makeOverListener(this.mapData, this.mapMarker, infowindow));
-			  kakao.maps.event.addListener(this.mapMarker, 'mouseout', makeOutListener(infowindow));
-			  //this.mapData = map;
+			  //kakao.maps.event.addListener(this.mapMarker, 'mouseleave', makeOutListener(infowindow));
+
 		  }
+		  */
+			  ////////////////여기 위로 그거 여기 아래로 오버레이
+			  ////////////////////////////////////////////////////////////
+			// 지도에 마커를 표시합니다
+
+
+				// 커스텀 오버레이에 표시할 컨텐츠 입니다
+				// 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
+				// 별도의 이벤트 메소드를 제공하지 않습니다
+
+			  for (let i = 0; i < this.myReviewData.length; i++) {
+				  var imageSrc = 'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
+						  imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+						  imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+				  var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+						  markerPosition = new kakao.maps.LatLng(this.myReviewData[i].lat, this.myReviewData[i].lon); // 마커가 표시될 위치입니다
+
+
+				  let mapMarker = new kakao.maps.Marker({
+					  //map: this.mapData,
+					  //position: new kakao.maps.LatLng(this.myReviewData[i].lat, this.myReviewData[i].lon)
+					  image: markerImage,
+					  position: markerPosition,
+				  });
+				  mapMarker.setMap(this.mapData);
+
+
+				  var content = '<div class="wrap" id="over' + i + '" style="display:none;">' +
+						  '    <div class="info">' +
+						  '        <div class="title">' +
+						  				this.myReviewData[i].store_name +
+//						  ' <div class="close" onclick="document.getElementById(\'closeOverlay\').value = \'' + this.myReviewData[i].store_id + '\';" title="닫기"></div>' +
+//alert("나 여깄어!");document.getElementById("closeOverlay").dispatchEvent(document.createEvent("HTMLEvents").initEvent("click", true, false));
+				  '            <div class="close" onclick="document.getElementById(\'over' + i + '\').style.display=\'none\';" title="닫기"></div>' +
+						  '        </div>' +
+						  '        <div class="body">' +
+						  '            <div class="img">' +
+						  '                <img src="http://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+						  '           </div>' +
+						  '            <div class="desc">' +
+						  '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+						  '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+						  '                <div><a href="http://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+						  '            </div>' +
+						  '        </div>' +
+						  '    </div>' +
+						  '</div>';
+
+					// 마커 위에 커스텀오버레이를 표시합니다
+					// 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+				  this.mapOverlay = new kakao.maps.CustomOverlay({
+					  content: content,
+					  map: this.mapData,
+					  position: mapMarker.getPosition()
+				  });
+
+				  //document.getElementById("over" + i).style.display = "none";
+				  // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+				  kakao.maps.event.addListener(mapMarker, 'click', function () {
+					  //this.mapOverlay.setMap(this.mapData);
+					  document.getElementById("over" + i).style.display = "block";
+
+				  });
+				 //kakao.maps.event.addListener(mapMarker, 'mouseout', function () {
+				//	  this.mapOverlay.setMap(null);
+				 // });
+			  }
+
+
+			  /*
+                            this.mapOverlay = new kakao.maps.CustomOverlay({
+                                content: positions[i].content,
+                                map: this.mapData,
+                                position: this.mapMarker.getPosition()
+                            });
+                        */
+
+			  /*
+			  kakao.maps.event.addListener(this.mapMarker, 'click', function() {
+				  this.mapOverlay.setMap(this.map);
+			  });
+			  kakao.maps.event.addListener(this.mapMarker, 'mouseout', function() {
+				  this.mapOverlay.setMap(null);
+			  });
+			  */
+
+
+
 
 /*
 		  this.mapMarker.setMap(null);
@@ -325,16 +439,8 @@ export default {
 								othersReviewData: []
 					  	 */
 						  vm.myReviewData = [];
-					  	alert("mydata 리스트 : " + response.data.mydata.length);
-						  for(let i = 0; i < response.data.mydata.length; i++){
+					  	  for(let i = 0; i < response.data.mydata.length; i++){
 						  	if(response.data.mydata[i].visit_yn == 'Y'){//나의 리뷰
-								//카텍좌표계 위경도로 변환
-								var geo = new GeoTrans();
-								geo.init("katec", "geo");
-								var pt = new Point(response.data.mydata[i].lon, response.data.mydata[i].lat);
-								var out_pt = geo.conv(pt);
-
-
 								vm.myReviewData.push({
 									store_id: response.data.mydata[i].store_id,
 									store_name: response.data.mydata[i].store_name,
@@ -342,10 +448,10 @@ export default {
 									telephone: response.data.mydata[i].telephone,
 									address: response.data.mydata[i].address,
 									roadaddress: response.data.mydata[i].roadaddress,
-									lon: out_pt.x,
+									lon: response.data.mydata[i].lon,
 									//mapx: out_pt.x,
 									//mapy: out_pt.y,
-									lat: out_pt.y,
+									lat: response.data.mydata[i].lat,
 									cnt: response.data.mydata[i].cnt,
 									ratingav: response.data.mydata[i].ratingav,
 									review_seq: response.data.mydata[i].review_seq,
@@ -360,7 +466,6 @@ export default {
 							}
 
 						  }
-						  alert("mydata 리스트 다넣었다 뷰는어때 : " + vm.myReviewData.length);
 						  vm.maptest();
 
 					  }
@@ -378,7 +483,10 @@ export default {
 // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
 function makeOverListener(map, marker, infowindow) {
 	return function() {
+
 		infowindow.open(map, marker);
+
+
 	};
 }
 
@@ -389,252 +497,6 @@ function makeOutListener(infowindow) {
 	};
 }
 
-
-
-
-var Point = function(x, y) {
-	this.x = x;
-	this.y = y;
-}
-
-var GeoTrans = function() {
-	this.srctype = "katec";
-	this.dsttype = "geo";
-	this.m_Ind = 0;
-	this.m_Es = 0;
-	this.m_Esp = 0;
-	this.src_m = 0;
-	this.dst_m = 0;
-
-	this.EPSLN = 0.0000000001;
-	this.m_arMajor = 6378137.0;
-	this.m_arMinor = 6356752.3142;
-
-	this.m_arScaleFactor = new Array();
-	this.m_arLonCenter = new Array();
-	this.m_arLatCenter = new Array();
-	this.m_arFalseNorthing = new Array();
-	this.m_arFalseEasting = new Array();
-};
-
-GeoTrans.prototype.init = function(srctype, dsttype) {
-	this.m_arScaleFactor["geo"] = 1;
-	this.m_arLonCenter["geo"] = 0.0;
-	this.m_arLatCenter["geo"] = 0.0;
-	this.m_arFalseNorthing["geo"] = 0.0;
-	this.m_arFalseEasting["geo"] = 0.0;
-
-	this.m_arScaleFactor["katec"] = 0.9999;
-	this.m_arLonCenter["katec"] = 2.23402144255274;
-	this.m_arLatCenter["katec"] = 0.663225115757845;
-	this.m_arFalseNorthing["katec"] = 600000.0;
-	this.m_arFalseEasting["katec"] = 400000.0;
-
-	this.srctype = srctype;
-	this.dsttype = dsttype;
-
-	var tmp = this.m_arMinor / this.m_arMajor;
-	this.m_Es = 1.0 - tmp * tmp;
-	this.m_Esp = this.m_Es / (1.0 - this.m_Es);
-
-	if(this.m_Es < 0.00001) {
-		this.m_Ind = 1.0;
-	} else {
-		this.m_Ind = 0.0;
-	}
-
-	this.src_m = this.m_arMajor * this.mlfn(this.e0fn(this.m_Es), this.e1fn(this.m_Es), this.e2fn(this.m_Es), this.e3fn(this.m_Es), this.m_arLatCenter[srctype]);
-	this.dst_m = this.m_arMajor * this.mlfn(this.e0fn(this.m_Es), this.e1fn(this.m_Es), this.e2fn(this.m_Es), this.e3fn(this.m_Es), this.m_arLatCenter[dsttype]);
-}
-
-GeoTrans.prototype.D2R = function(degree) {
-	return degree* Math.PI / 180.0;
-}
-
-GeoTrans.prototype.R2D = function(radian) {
-	return radian * 180.0 / Math.PI;
-}
-
-GeoTrans.prototype.e0fn = function(x) {
-	return 1.0 - 0.25 * x * (1.0 + x / 16.0 * (3.0 + 1.25 * x));
-}
-
-GeoTrans.prototype.e1fn = function(x) {
-	return 0.375 * x * (1.0 + 0.25 * x * (1.0 + 0.46875 * x));
-}
-
-GeoTrans.prototype.e2fn = function(x) {
-	return 0.05859375 * x * x * (1.0 + 0.75 * x);
-}
-
-GeoTrans.prototype.e3fn = function(x) {
-	return x * x * x * (35.0 / 3072.0);
-}
-
-GeoTrans.prototype.mlfn = function(e0, e1, e2, e3, phi) {
-	return e0 * phi - e1 * Math.sin(2.0 * phi) + e2 * Math.sin(4.0 * phi) - e3 * Math.sin(6.0 * phi);
-}
-
-GeoTrans.prototype.asinz = function(value) {
-	if(Math.abs(value) > 1.0) value = (value > 0 ? 1: -1);
-	return Math.asin(value);
-}
-
-GeoTrans.prototype.conv = function(in_pt) {
-	var inlon, inlat, outlon, outlat;
-
-	var tmpPt = new Point();
-	var out_pt = new Point();
-
-	if(this.srctype == "geo") {
-		tmpPt.x = this.D2R(in_pt.x);
-		tmpPt.y = this.D2R(in_pt.y);
-	} else {
-		this.tm2geo(in_pt, tmpPt);
-	}
-
-	outlon = inlon;
-	outlat = inlat;
-
-	if(this.dsttype == "geo") {
-		out_pt.x = this.R2D(tmpPt.x);
-		out_pt.y = this.R2D(tmpPt.y);
-	} else {
-		this.geo2tm(tmpPt, out_pt);
-		out_pt.x = Math.round(out_pt.x);
-		out_pt.y = Math.round(out_pt.y);
-	}
-
-	return out_pt;
-}
-
-GeoTrans.prototype.geo2tm = function(in_pt, out_pt) {
-	var delta_lon = in_pt.x - this.m_arLonCenter[this.dsttype];
-	var sin_phi = Math.sin(in_pt.y);
-	var cos_phi = Math.cos(in_pt.y);
-
-	if(this.m_Ind != 0) {
-		var b = cos_phi * Math.sin(delta_lon);
-
-		if((Math.abs(Math.abs(b) - 1.0)) < this.EPSLN) {
-			alert("무한대 에러");
-		}
-	} else {
-		var b = 0;
-		x = 0.5 * this.m_arMajor * this.m_arScaleFactor[this.dsttype] * Math.log((1.0 + b) / (1.0 - b));
-		var con = Math.acos(cos_phi * Math.cos(delta_lon) / Math.sqrt(1.0 - b * b));
-
-		if(in_pt.y < 0) {
-			con = con * -1;
-			y = this.m_arMajor * this.m_arScaleFactor[this.dsttype] * (con - this.m_arLatCenter[this.dsttype]);
-		}
-	}
-
-	var al = cos_phi * delta_lon;
-	var als = al * al;
-	var c = this.m_Esp * cos_phi * cos_phi;
-	var tq = Math.tan(in_pt.y);
-	var t = tq * tq;
-	var con = 1.0 - this.m_Es * sin_phi * sin_phi;
-	var n = this.m_arMajor / Math.sqrt(con);
-	var ml = this.m_arMajor * this.mlfn(this.e0fn(this.m_Es), this.e1fn(this.m_Es), this.e2fn(this.m_Es), this.e3fn(this.m_Es), in_pt.y);
-
-	out_pt.x = this.m_arScaleFactor[this.dsttype] * n * al * (1.0 + als / 6.0 * (1.0 - t + c + als / 20.0 * (5.0 - 18.0 * t + t * t + 72.0 * c - 58.0 * this.m_Esp))) + this.m_arFalseEasting[this.dsttype];
-	out_pt.y = this.m_arScaleFactor[this.dsttype] * (ml - this.dst_m + n * tq * (als * (0.5 + als / 24.0 * (5.0 - t + 9.0 * c + 4.0 * c * c + als / 30.0 * (61.0 - 58.0 * t + t * t + 600.0 * c - 330.0 * this.m_Esp))))) + this.m_arFalseNorthing[this.dsttype];
-}
-
-
-GeoTrans.prototype.tm2geo = function(in_pt, out_pt) {
-	var max_iter = 6;
-
-	if(this.m_Ind != 0) {
-		var f = Math.exp(in_pt.x / (this.m_arMajor * this.m_arScaleFactor[this.srctype]));
-		var g = 0.5 * (f - 1.0 / f);
-		var temp = this.m_arLatCenter[this.srctype] + in_pt.y / (this.m_arMajor * this.m_arScaleFactor[this.srctype]);
-		var h = Math.cos(temp);
-		var con = Math.sqrt((1.0 - h * h) / (1.0 + g * g));
-		out_pt.y = asinz(con);
-
-		if(temp < 0) out_pt.y *= -1;
-
-		if((g == 0) && (h == 0)) {
-			out_pt.x = this.m_arLonCenter[this.srctype];
-		} else {
-			out_pt.x = Math.atan(g / h) + this.m_arLonCenter[this.srctype];
-		}
-	}
-
-	in_pt.x -= this.m_arFalseEasting[this.srctype];
-	in_pt.y -= this.m_arFalseNorthing[this.srctype];
-
-	var con = (this.src_m + in_pt.y / this.m_arScaleFactor[this.srctype]) / this.m_arMajor;
-	var phi = con;
-
-	var i = 0;
-
-	while(true) {
-		var delta_Phi = ((con + this.e1fn(this.m_Es) * Math.sin(2.0 * phi) - this.e2fn(this.m_Es) * Math.sin(4.0 * phi) + this.e3fn(this.m_Es) * Math.sin(6.0 * phi)) / this.e0fn(this.m_Es)) - phi;
-		phi = phi + delta_Phi;
-
-		if(Math.abs(delta_Phi) <= this.EPSLN) break;
-
-		if(i >= max_iter) {
-			alert("무한대 에러");
-			break;
-		}
-
-		i++;
-	}
-
-	if(Math.abs(phi) < (Math.PI / 2)) {
-		var sin_phi = Math.sin(phi);
-		var cos_phi = Math.cos(phi);
-		var tan_phi = Math.tan(phi);
-		var c = this.m_Esp * cos_phi * cos_phi;
-		var cs = c * c;
-		var t = tan_phi * tan_phi;
-		var ts = t * t;
-		var con = 1.0 - this.m_Es * sin_phi * sin_phi;
-		var n = this.m_arMajor / Math.sqrt(con);
-		var r = n * (1.0 - this.m_Es) / con;
-		var d = in_pt.x / (n * this.m_arScaleFactor[this.srctype]);
-		var ds = d * d;
-		out_pt.y = phi - (n * tan_phi * ds / r) * (0.5 - ds / 24.0 * (5.0 + 3.0 * t + 10.0 * c - 4.0 * cs - 9.0 * this.m_Esp - ds / 30.0 * (61.0 + 90.0 * t + 298.0 * c + 45.0 * ts - 252.0 * this.m_Esp - 3.0 * cs)));
-		out_pt.x = this.m_arLonCenter[this.srctype] + (d * (1.0 - ds / 6.0 * (1.0 + 2.0 * t + c - ds / 20.0 * (5.0 - 2.0 * c + 28.0 * t - 3.0 * cs + 8.0 * this.m_Esp + 24.0 * ts))) / cos_phi);
-	} else {
-		out_pt.y = Math.PI * 0.5 * Math.sin(in_pt.y);
-		out_pt.x = this.m_arLonCenter[this.srctype];
-	}
-}
-
-GeoTrans.prototype.getDistancebyGeo = function(pt1, pt2) {
-	var lat1 = this.D2R(pt1.y);
-	var lon1 = this.D2R(pt1.x);
-	var lat2 = this.D2R(pt2.y);
-	var lon2 = this.D2R(pt2.x);
-
-	var longitude = lon2 - lon1;
-	var latitude = lat2 - lat1;
-
-	var a = Math.pow(Math.sin(latitude / 2.0), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(longitude / 2.0), 2);
-	return 6376.5 * 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0 - a));
-}
-
-GeoTrans.prototype.getDistancebyKatec = function(pt1, pt2) {
-	var geo = new GeoTrans("katec", "geo");
-	pt1 = geo.conv(pt1);
-	pt2 = geo.conv(pt2);
-
-	return this.getDistancebyGeo(pt1, pt2);
-}
-
-GeoTrans.prototype.getTimebySec = function(distance) {
-	return Math.round(3600 * distance / 4);
-}
-
-GeoTrans.prototype.getTimebyMin = function(distance) {
-	return Number(Math.ceil(this.getTimebySec(distance) / 60));
-}
 
 </script>
 <style></style>
