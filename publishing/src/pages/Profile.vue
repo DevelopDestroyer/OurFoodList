489a6e27befc1014bc9c82213e1902cd066fbc35
developdestroyer <developdestroyer@gmail.com>
2020-05-02 15:09:45 +0900

    프로필페이지 퍼블리싱

--- publishing/src/pages/Profile.vue
+++ publishing/src/pages/Profile.vue
@@ -10,20 +10,16 @@
         <div class="photo-container">
           <img src="img/avatar/8.png" alt=""/>
         </div>
-        <h3 class="title">Ryan Scheinder</h3>
-        <p class="category">Photographer</p>
+        <h3 class="title">이거좀먹어봐</h3>
+        <p class="category">개발자</p>
         <div class="content">
           <div class="social-description">
-            <h2>26</h2>
-            <p>Comments</p>
+            <h2>26/126</h2>
+            <p>리뷰/찜</p>
           </div>
           <div class="social-description">
-            <h2>26</h2>
-            <p>Comments</p>
-          </div>
-          <div class="social-description">
-            <h2>48</h2>
-            <p>Bookmarks</p>
+            <h2>35/1023</h2>
+            <p>팔로잉/팔로워</p>
           </div>
         </div>
       </div>
@@ -49,7 +45,33 @@
             <i class="fab fa-instagram"></i>
           </a>
         </div>
-        <h3 class="title">About me</h3>
+        <!--h3 class="title">About me</h3-->
+        <!--div class="row">
+          <tabs type="primary">
+            <tab-pane label="리뷰">
+              리뷰리스트가 들어갈 자리... 남일때만 보임
+            </tab-pane>
+            <tab-pane label="찜">
+              찜리스트가 들어갈 자리... 남일때만 보임
+            </tab-pane>
+            <tab-pane label="방명록">
+              방명록은<br>
+              모두에게 보인다
+            </tab-pane>
+            <tab-pane label="친구들">
+              <div class="row">
+                친구들 리스트..<br>
+                팔로잉/팔로워..
+              </div>
+            </tab-pane>
+            <tab-pane label="계정관리">
+              아바타설정<br>
+              비번변경<br>
+              탈퇴기능
+            </tab-pane>
+          </tabs>
+        </div-->
+
         <h5 class="description">
           An artist of considerable range, Ryan — the name taken by
           Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
@@ -58,7 +80,7 @@
         </h5>
         <div class="row">
           <div class="col-md-6 ml-auto mr-auto">
-            <h4 class="title text-center">My Portfolio</h4>
+            <h4 class="title text-center">프로필</h4>
           </div>
           <tabs
             pills
@@ -68,7 +90,9 @@
             type="primary"
           >
             <tab-pane title="Profile">
-              <i slot="label" class="now-ui-icons design_image"></i>
+              <div slot="label">
+                <i class="now-ui-icons location_bookmark"></i>리뷰/찜
+              </div>
 
               <div class="col-md-10 ml-auto mr-auto">
                 <div class="row collections">
@@ -85,7 +109,9 @@
             </tab-pane>
 
             <tab-pane title="Home">
-              <i slot="label" class="now-ui-icons location_world"></i>
+              <div slot="label">
+                <i class="now-ui-icons ui-2_chat-round"></i>방명록
+              </div>
 
               <div class="col-md-10 ml-auto mr-auto">
                 <div class="row collections">
@@ -102,7 +128,9 @@
             </tab-pane>
 
             <tab-pane title="Messages">
-              <i slot="label" class="now-ui-icons sport_user-run"></i>
+              <div slot="label">
+                <i class="now-ui-icons users_single-02"></i>친구들
+              </div>
 
               <div class="col-md-10 ml-auto mr-auto">
                 <div class="row collections">
@@ -117,6 +145,26 @@
                 </div>
               </div>
             </tab-pane>
+
+            <tab-pane title="Messages">
+              <div slot="label">
+                <i class="now-ui-icons ui-1_settings-gear-63"></i>계정관리
+              </div>
+
+              <div class="col-md-10 ml-auto mr-auto">
+                <div class="row collections">
+                  <div class="col-md-6">
+                    <img src="img/bg3.jpg" alt="" class="img-raised" />
+                    <img src="img/bg8.jpg" alt="" class="img-raised" />
+                  </div>
+                  <div class="col-md-6">
+                    <img src="img/bg7.jpg" alt="" class="img-raised" />
+                    <img src="img/bg6.jpg" class="img-raised" />
+                  </div>
+                </div>
+              </div>
+            </tab-pane>
+
           </tabs>
         </div>
       </div>
